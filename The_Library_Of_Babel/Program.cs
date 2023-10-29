bf861a6c4a27a4d7c2d83f4dd6f1719c9af8e49a
Звездемир Пенев <zvezdibaronz@gmail.com>
2023-10-29 15:49:32 +0200

    Added some UI in the Program.cs

--- The_Library_Of_Babel/Program.cs
+++ The_Library_Of_Babel/Program.cs
@@ -9,6 +9,16 @@ namespace The_Library_Of_Babel
         {
             List<Book> books = new List<Book>();
             List<Reader> readers = new List<Reader>();
+
+            Console.WriteLine("WELCOME TO THE LIBRARY OF BABEL\n");
+            Console.WriteLine("List of commands:");
+
+            Console.WriteLine("[1] Add a book");
+            Console.WriteLine("[2] Add a reader");
+            Console.WriteLine("[3] Take a book\n");
+
+            Console.Write("Enter the code of your command: ");
+            int n = int.Parse(Console.ReadLine());
         }
     }
 }
