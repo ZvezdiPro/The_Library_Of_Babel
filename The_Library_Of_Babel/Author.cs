3b70f05cff44d4136e927137406325a8e5813f06
Звездемир Пенев <zvezdibaronz@gmail.com>
2023-10-29 15:05:28 +0200

    Add project files.

---
+++ The_Library_Of_Babel/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Library_Of_Babel
+{
+    internal class Author
+    {
+    }
+}
---
+++ The_Library_Of_Babel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace The_Library_Of_Babel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
